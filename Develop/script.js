2461d3db098a1683f6486ba1e5e9fb03f5a7ca9d
V Mendez <victormendez990@gmail.com>
2022-10-04 15:43:18 -0600

    Create and Start functions in JS

---
+++ Develop/script.js
@@ -0,0 +1,18 @@
+// Variables to define
+var currentDay = $("#currentDay");
+
+// Init function to call all necessary functions at page load
+function init(){
+    
+}
+
+// function to set the current date in Jumbotron
+function setCurrentDate(){
+
+}
+
+// function to set the background color based on time
+function setColorCode(){
+
+}
+
