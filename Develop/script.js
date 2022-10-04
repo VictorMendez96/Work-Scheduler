0f34f74fc447eccbb96454e72f4638bc47a7833c
V Mendez <victormendez990@gmail.com>
2022-10-04 15:53:04 -0600

    Draft all functions/ add event listener for button

--- Develop/script.js
+++ Develop/script.js
@@ -1,18 +1,37 @@
 // Variables to define
 var currentDay = $("#currentDay");
+var sav = $("#saveBtn");
+
 
 // Init function to call all necessary functions at page load
 function init(){
-    
-}
+    setCurrentDate();
+    setColorCode;
+    showEvents();
+};
 
 // function to set the current date in Jumbotron
 function setCurrentDate(){
 
-}
+};
 
 // function to set the background color based on time
 function setColorCode(){
 
-}
+};
 
+// function to save input events
+function saveEvent(){
+
+};
+
+// function to show saved events
+function showEvents(){
+
+};
+
+// Create event listener to save input event
+saveEvent.click(saveEvent);
+
+// Call init() function
+init();
